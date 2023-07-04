fa9f1e05ea314004ffa8e250623cb9232a8bb3fe
WoodyXP <stiefelwoods@gmail.com>
2023-07-04 17:36:23 +0200

    Updated the navalunitdb and added new type range into unit types

--- client/src/units/navalunitdatabase.ts
+++ client/src/units/navalunitdatabase.ts
@@ -4,12 +4,12 @@ export class NavalDatabase extends UnitDatabase {
    constructor() {
       super();
       this.blueprints = {
-            "CVN-70 Carl Vinson": {
-                "name": "CVN-70 Carl Vinson",
-                "class": "Aircraft Carrier",
+            "052B DDG-168 Guangzhou": {
+                "name": "052B DDG-168 Guangzhou",
+                "type": "Modern",
                 "era": ["Mid Cold War"],
-                "label": "CVN-70 Carl Vinson",
-                "shortLabel": "CVN-70 Carl Vinson",
+                "label": "052B DDG-168 Guangzhou",
+                "shortLabel": "052B DDG-168 Guangzhou",
                 "range": "Short",
                 "loadouts": [
                     {
@@ -23,7 +23,467 @@ export class NavalDatabase extends UnitDatabase {
                     }
                 ],
                 "filename": ""
-            },
+          },
+          "052C DDG-171 Haikou": {
+               "name": "052C DDG-171 Haikou",
+               "type": "Destroyer",
+               "era": ["Modern"],
+               "label": "052C DDG-171 Haikou",
+               "shortLabel": "052C DDG-171 Haikou",
+               "range": "Short",
+               "loadouts": [
+                   {
+                       "fuel": 1,
+                       "items": [],
+                       "roles": [
+                           "Template"
+                       ],
+                       "code": "",
+                       "name": "Default"
+                   }
+               ],
+               "filename": ""
+          },
+          "054A FFG-538 Yantai": {
+              "name": "054A FFG-538 Yantai",
+              "type": "Frigate",
+              "era": ["Modern"],
+              "label": "054A FFG-538 Yantai",
+              "shortLabel": "054A FFG-538 Yantai",
+              "range": "Medium",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Type 071": {
+              "name": "Type 071",
+              "type": "Transport Dock",
+              "era": ["Modern"],
+              "label": "Type 071",
+              "shortLabel": "Type 071",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Type 093": {
+              "name": "Type 093",
+              "type": "Submarine",
+              "era": ["Modern"],
+              "label": "Type 093",
+              "shortLabel": "Type 093",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Akizuki": {
+              "name": "Akizuki",
+              "type": "Destroyer",
+              "era": ["WW2"],
+              "label": "Akizuki",
+              "shortLabel": "Akizuki",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "ARA Santa Fe S-21": {
+              "name": "ARA Santa Fe S-21",
+              "type": "Submarine",
+              "era": ["Early Cold War"],
+              "label": "ARA Santa Fe S-21",
+              "shortLabel": "ARA Santa Fe S-21",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "ARA Vienticinco de Mayo": {
+              "name": "ARA Vienticinco de Mayo",
+              "type": "Aircraft Carrier",
+              "era": ["Mid Cold War"],
+              "label": "ARA Vienticinco de Mayo",
+              "shortLabel": "ARA Vienticinco de Mayo",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Admiral Kuznetsov": {
+              "name": "Admiral Kuznetsov",
+              "type": "Aircraft Carrier",
+              "era": ["Late Cold War"],
+              "label": "Admiral Kuznetsov",
+              "shortLabel": "Admiral Kuznetsov",
+              "range": "Medium",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Albatros (Grisha-5)": {
+              "name": "Albatros (Grisha-5)",
+              "type": "Aircraft Carrier",
+              "era": ["Early Cold War"],
+              "label": "Albatros (Grisha-5)",
+              "shortLabel": "Albatros (Grisha-5)",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Almirante Condell PFG-06": {
+              "name": "Almirante Condell PFG-06",
+              "type": "Frigate",
+              "era": ["Mid Cold War"],
+              "label": "Almirante Condell PFG-06",
+              "shortLabel": "Almirante Condell PFG-06",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Almirante lynch PFG-07": {
+              "name": "Almirante lynch PFG-07",
+              "type": "Frigate",
+              "era": ["Mid Cold War"],
+              "label": "Almirante lynch PFG-07",
+              "shortLabel": "Almirante lynch PFG-07",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Boat Armed Hi-Speed": {
+              "name": "Boat Armed Hi-Speed",
+              "type": "Fast Attack Craft",
+              "era": ["Mid Cold War"],
+              "label": "Boat Armed Hi-Speed",
+              "shortLabel": "Boat Armed Hi-Speed",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Boat LCVP Higgins": {
+              "name": "Boat LCVP Higgins",
+              "type": "Landing Craft",
+              "era": ["WW2"],
+              "label": "Boat LCVP Higgins",
+              "shortLabel": "Boat LCVP Higgins",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Boat Schnellboot type S130": {
+              "name": "Boat Schnellboot type S130",
+              "type": "Fast Attack Craft",
+              "era": ["WW2"],
+              "label": "Boat Schnellboot type S130",
+              "shortLabel": "Boat Schnellboot type S130",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "Bulker Handy Wind": {
+              "name": "Bulker Handy Wind",
+              "type": "Cargoship",
+              "era": ["Late Cold War"],
+              "label": "Bulker Handy Wind",
+              "shortLabel": "Bulker Handy Wind",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CV 1143.5 Admiral Kuznetsov(2017)": {
+              "name": "CV 1143.5 Admiral Kuznetsov(2017)",
+              "type": "Aircraft Carrier",
+              "era": ["Modern"],
+              "label": "CV 1143.5 Admiral Kuznetsov(2017)",
+              "shortLabel": "CV 1143.5 Admiral Kuznetsov(2017)",
+              "range": "Medium",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CV-59 Forrestal": {
+              "name": "CV-59 Forrestal",
+              "type": "Aircraft Carrier",
+              "era": ["Early Cold War"],
+              "label": "CV-59 Forrestal",
+              "shortLabel": "CV-59 Forrestal",
+              "range": "Short",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CV6 USS Enterprise": {
+              "name": "CV6 USS Enterprise -The Grey Ghost-",
+              "type": "Aircraft Carrier",
+              "era": ["WW2"],
+              "label": "CV6 USS Enterprise -The Grey Ghost-",
+              "shortLabel": "CV6 USS Enterprise",
+              "range": "",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CVN-71 Theodore Roosevelt": {
+              "name": "CVN-71 Theodore Roosevelt",
+              "type": "Super Aircraft Carrier",
+              "era": ["Late Cold War"],
+              "label": "CVN-71 Theodore Roosevelt",
+              "shortLabel": "CVN-71 Theodore Roosevelt",
+              "range": "Short",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CVN-72 Abraham Lincoln": {
+              "name": "CVN-72 Abraham Lincoln",
+              "type": "Super Aircraft Carrier",
+              "era": ["Late Cold War"],
+              "label": "CVN-72 Abraham Lincoln",
+              "shortLabel": "CVN-72 Abraham Lincoln",
+              "range": "Short",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CVN-73 George Washington": {
+              "name": "CVN-73 George Washington",
+              "type": "Super Aircraft Carrier",
+              "era": ["Late Cold War"],
+              "label": "CVN-73 George Washington",
+              "shortLabel": "CVN-73 George Washington",
+              "range": "Medium",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CVN-74 John C. Stennis": {
+              "name": "CVN-74 John C. Stennis",
+              "type": "Aircraft Carrier",
+              "era": ["Late Cold War"],
+              "label": "CVN-74 John C. Stennis",
+              "shortLabel": "CVN-74 John C. Stennis",
+              "range": "Medium",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
+          "CVN-75 Harry S. Truman": {
+              "name": "CVN-75 Harry S. Truman",
+              "type": "Aircraft Carrier",
+              "era": ["Late Cold War"],
+              "label": "CVN-75 Harry S. Truman",
+              "shortLabel": "CVN-75 Harry S. Truman",
+              "range": "Medium",
+              "loadouts": [
+                  {
+                      "fuel": 1,
+                      "items": [],
+                      "roles": [
+                          "Template"
+                      ],
+                      "code": "",
+                      "name": "Default"
+                  }
+              ],
+              "filename": ""
+          },
       }
    }
 }
