3db5202cdff1723c7074ec8b3affd5f9b723b06f
PeekabooSteam <matthew.lindley001+github@gmail.com>
2023-10-01 20:53:30 +0100

    User now alerted when server is paused.

--- client/src/panels/connectionstatuspanel.ts
+++ client/src/panels/connectionstatuspanel.ts
@@ -1,12 +1,36 @@
+import { getApp } from "..";
 import { Panel } from "./panel";
 
 export class ConnectionStatusPanel extends Panel {
+
+    #previousMissionElapsedTime:number = 0;
+
     constructor(ID: string) {
         super( ID );
     }
 
     
     update(connected: boolean) {
-        this.getElement().toggleAttribute( "data-is-connected", connected );
+
+        if ( connected ) {
+
+            const missionElapsedTime = getApp().getMissionManager().getDateAndTime().elapsedTime;
+    
+            if ( missionElapsedTime === this.#previousMissionElapsedTime ) {
+                this.getElement().toggleAttribute( "data-is-connected", false );
+                this.getElement().toggleAttribute( "data-is-paused", true );
+            } else {
+                this.getElement().toggleAttribute( "data-is-connected", true );
+                this.getElement().toggleAttribute( "data-is-paused", false );
+            }
+
+            this.#previousMissionElapsedTime = missionElapsedTime;
+
+        } else {
+
+            this.getElement().toggleAttribute( "data-is-connected", false );
+
+        }
+
     }
 }
