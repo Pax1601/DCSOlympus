a82c033da038cbacf13ecf76813f6b2edfdf5f3e
PeekabooSteam <matthew.lindley001+github@gmail.com>
2023-10-02 07:15:00 +0100

    Timeout much shorter when server is paused to reduce update lag

--- client/src/panels/connectionstatuspanel.ts
+++ client/src/panels/connectionstatuspanel.ts
@@ -1,36 +1,27 @@
-import { getApp } from "..";
 import { Panel } from "./panel";
 
 export class ConnectionStatusPanel extends Panel {
-
-    #previousMissionElapsedTime:number = 0;
 
     constructor(ID: string) {
         super( ID );
     }
 
-    
-    update(connected: boolean) {
 
-        if ( connected ) {
-
-            const missionElapsedTime = getApp().getMissionManager().getDateAndTime().elapsedTime;
-    
-            if ( missionElapsedTime === this.#previousMissionElapsedTime ) {
-                this.getElement().toggleAttribute( "data-is-connected", false );
-                this.getElement().toggleAttribute( "data-is-paused", true );
-            } else {
-                this.getElement().toggleAttribute( "data-is-connected", true );
-                this.getElement().toggleAttribute( "data-is-paused", false );
-            }
-
-            this.#previousMissionElapsedTime = missionElapsedTime;
-
-        } else {
-
-            this.getElement().toggleAttribute( "data-is-connected", false );
-
-        }
-
+    showDisconnected() {
+        this.getElement().toggleAttribute( "data-is-connected", false );
+        this.getElement().toggleAttribute( "data-is-paused", false );
     }
+
+
+    showConnected() {
+        this.getElement().toggleAttribute( "data-is-connected", true );
+        this.getElement().toggleAttribute( "data-is-paused", false );
+    }
+
+
+    showServerPaused() {
+        this.getElement().toggleAttribute( "data-is-connected", false );
+        this.getElement().toggleAttribute( "data-is-paused", true );
+    }
+    
 }
