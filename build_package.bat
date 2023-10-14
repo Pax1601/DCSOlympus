1a579c5755eaa1077134624fef6588300c4b8488
Pax1601 <d.passoni92@gmail.com>
2023-10-14 12:13:32 +0200

    Build scripts updated

--- build_package.bat
+++ build_package.bat
@@ -1,4 +1,11 @@
+cd src
+msbuild olympus.sln /t:Rebuild /p:Configuration=Release
+cd ..
 cd client
+call npm install
+rmdir /s /q "hgt"
+call npm run emit-declarations
+call npm run build
 cd "plugins\controltips"
 call npm run build
 cd "..\.."
