955057183d9247ecaec8e387af99a95fea748445
PeekabooSteam <matthew.lindley001+github@gmail.com>
2023-12-27 16:26:31 +0000

    Added JSON validation for imports.

--- client/src/schemas/schema.ts
+++ client/src/schemas/schema.ts
@@ -1,18 +1,43 @@
-import Ajv, { JSONSchemaType } from "ajv";
-import { AnySchemaObject, Schema } from "ajv/dist/core";
+import Ajv from "ajv";
+import { AnySchemaObject } from "ajv/dist/core";
 
 
 //  For future extension
 abstract class JSONSchemaValidator {
 
+    #ajv:Ajv;
+    #compiledValidator:any;
     #schema!:AnySchemaObject;
 
     constructor( schema:AnySchemaObject ) {
         this.#schema = schema;
+
+        this.#ajv = new Ajv({
+            "allErrors": true
+        });
+
+        this.#compiledValidator = this.getAjv().compile(this.getSchema());
+
+    }
+
+    getAjv() {
+        return this.#ajv;
+    }
+
+    getCompiledValidator() {
+        return this.#compiledValidator;
+    }
+
+    getErrors() {
+        return this.getCompiledValidator().errors;
     }
 
     getSchema() {
         return this.#schema;
+    }
+
+    validate(data:any) {
+        return (this.getCompiledValidator())(data);
     }
 
 }
@@ -20,15 +45,11 @@ abstract class JSONSchemaValidator {
 
 export class AirbasesJSONSchemaValidator extends JSONSchemaValidator {
 
-    constructor( ) {
+    constructor() {
 
         const schema = require("../schemas/airbases.schema.json");
 
         super( schema );
-
-        const ajv = new Ajv({
-            "allErrors": true
-        });
         
         [
             require( "../../public/databases/airbases/caucasus.json" ),
@@ -41,11 +62,21 @@ export class AirbasesJSONSchemaValidator extends JSONSchemaValidator {
             require( "../../public/databases/airbases/syria.json" ),
             require( "../../public/databases/airbases/thechannel.json" )
         ].forEach( data => {
-            const validate = ajv.compile(this.getSchema());
+            const validate = this.getAjv().compile(this.getSchema());
             const valid = validate(data);
             
             if (!valid) console.error(validate.errors);
         });
+    }
+
+}
+
+
+export class ImportFileJSONSchemaValidator extends JSONSchemaValidator {
+
+    constructor() {
+        const schema = require("../schemas/importdata.schema.json");
+        super( schema );
     }
 
 }
