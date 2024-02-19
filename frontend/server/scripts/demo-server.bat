39c28ffb0419e2840727f68382a955c40bcd53c4
Davide Passoni <d.passed@libero.it>
2024-02-19 16:45:55 +0100

    Added problem matchers

--- frontend/server/scripts/demo-server.bat
+++ frontend/server/scripts/demo-server.bat
@@ -1,7 +1,3 @@
-cd ..
-call .\check_setup.bat 
-cd server
-
 call .\scripts\copy.bat
 call .\scripts\create-mock-dcs.bat
 node .\bin\demo --config %1
--- frontend/website/scripts/watch.bat
+++ frontend/website/scripts/watch.bat
@@ -1,5 +1,1 @@
-cd ..
-call .\check_setup.bat 
-cd website
-
-watchify .\src\index.ts --debug -o ..\server\public\javascripts\bundle.js -t [ babelify --global true --presets [ @babel/preset-env ] --extensions '.js'] -p [ tsify --noImplicitAny ]
+watchify .\src\index.ts --debug -o ..\server\public\javascripts\bundle.js -t [ babelify --global true --presets [ @babel/preset-env ] --extensions '.js'] -p [ tsify --noImplicitAny ] -v
