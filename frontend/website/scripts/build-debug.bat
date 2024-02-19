6373fd300d427367d780e99f9b0fa96ddb7c44d2
Davide Passoni <d.passed@libero.it>
2024-02-19 15:52:20 +0100

    Fixed output for build debug

--- frontend/website/scripts/build-debug.bat
+++ frontend/website/scripts/build-debug.bat
@@ -1,1 +1,1 @@
-call browserify --debug  .\src\index.ts -o ..\..\build\frontend\public\javascripts\bundle.js -t [ babelify --global true --presets [ @babel/preset-env ] --extensions '.js'] -p [ tsify --noImplicitAny ]
+call browserify --debug  .\src\index.ts -o ..\server\public\javascripts\bundle.js -t [ babelify --global true --presets [ @babel/preset-env ] --extensions '.js'] -p [ tsify --noImplicitAny ]
